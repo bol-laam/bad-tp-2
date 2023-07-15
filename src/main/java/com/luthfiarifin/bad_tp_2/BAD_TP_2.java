20d1b0df2c36b39c1328aeffada6e3ea832188d9
Luthfi Arifin <luthfiarifin0222@gmail.com>
2023-07-15 16:20:34 +0700

    Initial commit

---
+++ src/main/java/com/luthfiarifin/bad_tp_2/BAD_TP_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.luthfiarifin.bad_tp_2;
+
+/**
+ *
+ * @author luthfiarifin
+ */
+public class BAD_TP_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
